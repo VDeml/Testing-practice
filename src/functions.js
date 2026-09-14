5fffa5dc204cf3455df6b2f98590578f942e3f46
Vojtěch Deml <235454976+VDeml@users.noreply.github.com>
2026-09-14 21:49:55 +0200

    Add calculator object with all tests

--- src/functions.js
+++ src/functions.js
@@ -7,5 +7,18 @@ function reverseString(string) {
     return string.split("").reverse().join("")
 }
 
-
-export { capitalize, reverseString };
+const calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b;
+    },
+    divide: function(a,b) {
+        return a / b;
+    },
+    multiply: function(a,b) {
+        return a * b;
+    }
+}
+export { capitalize, reverseString, calculator };
--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { capitalize, reverseString } from "../src/functions.js"
+import { capitalize, reverseString, calculator } from "../src/functions.js"
 
 test("capitalize hello to Hello", () => {
   expect(capitalize("hello")).toBe("Hello");
@@ -8,3 +8,19 @@ test("capitalize hello to Hello", () => {
 test("reverse World to dlroW", () => {
     expect(reverseString("World")).toBe("dlroW")
 });
+
+test("calculator add 1 + 2", () => {
+    expect(calculator.add(1, 2)).toBe(3)
+})
+
+test("calculator subtract 2 - 1", () => {
+    expect(calculator.subtract(2, 1)).toBe(1)
+})
+
+test("calculator divide 4 / 2", () => {
+    expect(calculator.divide(4, 2)).toBe(2)
+})
+
+test("calculator multiply 2 * 2", () => {
+    expect(calculator.multiply(2, 2)).toBe(4)
+})
